fc9e72df6d7bbdbba80199346c358ed9e5650c5d
Nancy <agarwalnancy1603@gmail.com>
2022-01-08 01:52:10 +0530

    preloader added

---
+++ src/PreLoader.js
@@ -0,0 +1,10 @@
+const PreLoader = () => {
+    return ( 
+        <div className="center">
+        <div className="ring"></div>
+        <span className="loading">loading...</span>
+     </div>
+     );
+}
+ 
+export default PreLoader;
