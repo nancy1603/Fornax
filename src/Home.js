03a33c91fc4b1170ec2620a7d90779232d7f7150
Nancy <agarwalnancy1603@gmail.com>
2022-01-08 01:10:43 +0530

    navbar added

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar';
+
+
+const Home = () => {
+    return (
+        <div>
+                <Navbar />
+                    
+        </div>
+    );
+}
+
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,28 @@
+import {Link} from "react-router-dom";
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+function Navbar() {
+    return(
+      
+       <div className="navbar">
+        <div className="logo">FORNAX</div>
+        <input type="checkbox" id="click" />
+        <label for="click" className="menu-btn">
+           <FontAwesomeIcon icon={faBars} className="i"></FontAwesomeIcon>
+        </label>
+    
+    <ul>
+       
+         <li><a href="#complain">Complain</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#timeline">Timeline</a></li>
+        <li><a href="#team">Team</a></li>
+        <li><a href="#logout">Logout</a></li>
+       
+    </ul>
+    </div>
+   
+    );
+}
+
+export default Navbar;
